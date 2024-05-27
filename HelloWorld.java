0362d6dd5753ac55b834745b1dcd9bd51dc850f7
Atul Kahate <newdelthis@gmail.com>
2024-05-27 16:23:23 +0530

    Bug fixing D1 done

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println ("Hello, World!");
+		System.out.println ("Hello World from C-DAC ACTS!");
+                System.out.println ("D-1");
 	}
 }
 
